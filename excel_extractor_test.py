68c78733e93a60593749cae1571172a58a864572
RicheyShaw <rickyshao1988@foxmail.com>
2018-02-13 19:38:25 +0800

    Update excel_extractor_test.py

--- excel_extractor_test.py
+++ excel_extractor_test.py
@@ -1,29 +1,55 @@
-# 
-# auto excel processing by SLQ
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import os
-import openpyxl # Is it different from "from openpyxl import Workbook" 
-# from openpyxl import Workbook
+# readCensusExcel.py - calculate population by SLQ
+# for each county
+import openpyxl, pprint, os
+from openpyxl import Workbook
+
+
+# from openpyxl.cell.cell import column_index_from_string
 
 os.chdir(r"C:\Users\shaolqi\Desktop")
+print('Opening workbook...')
+wb = openpyxl.load_workbook('免征车辆购置税的新能源汽车车型目录（第十五批）.xlsx')
+sheet = wb.get_sheet_by_name('Table 1')
+# ------------create a new workbook to store the selected data
+wbTemp = Workbook()
+sheetTemp = wbTemp.create_sheet('newEnergyCarList', 0)
+# print(type(sheet))
 
-wb = openpyxl.load_workbook('example.xlsx')
+# ------------------------#customize the rectangle area you want-----------------------
+# startRow    = int(input("enter the start row number you want:"))
+# startColumn = input("enter the start column letter you want:")
+# endRow      = int(input("enter the end row number you want:"))
+# endColumn   = input("enter the end column letter you want:")
+#
+# startCell = startColumn + str(startRow)
+# endCell   = endColumn + str(endRow)
+startCell = 'A5'
+endCell = 'L28'
+#
+recArea = sheet[startCell:endCell]  # recArea type is tuple
+# listRecArea = list(recArea)
 
-sheet = wb.get_sheet_by_name('Sheet1')
-#---------------------------------------------------------------------------------
-# choose the range of table you want
-r_want = int(input('enter row number you want:'))       
-c_want = int(input('enter column number you want:'))   
-# tuple(sheet['A1':'C3'])
+# print(recArea[0][1].value)
+# Loop through recArea --------------------------------------------------------------------------------------
+dictData = {}
+lsEachDictKey = []
 
-print(sheet.cell(row=r_want,column=c_want).value)
-# for i in range(1,8):
-#     print(i,sheet.cell(row=i,column=3).value)
-    
-# for i in range(sheet.max_row,sheet.max_column,2):
-#     print (i,sheet.cell(row=i,column=2).value)
+lsEachDictValue = []
 
-# for rowOfCellObjects in sheet['A1':'C3']:
-#     for cellObj in rowOfCellObjects:
-#         print(cellObj.coordinate, cellObj.value)
-#     print('----END of ROW----')
+for rowNum in range(0,len(recArea)):                     #iterate all row in recArea
+    if rowNum == 0: #如果是第一行，则存为key
+        for eachCell in recArea[rowNum]:
+            lsEachDictKey.append(eachCell.value)
+        tupEachDictKey = tuple(lsEachDictKey)
+    else:
+        for eachCell in recArea[rowNum]:
+            lsEachDictValue.append(eachCell.value)
+    dictData.setdefault(tupEachDictKey,lsEachDictValue)
+
+pprint.pprint(dictData)
+
+
+# wbTemp.save('newEnergyCarList')
