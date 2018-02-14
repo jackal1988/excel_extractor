4288be96ecee729b84e8bd0b519bf4b9e8d0ebcd
RicheyShaw <rickyshao1988@foxmail.com>
2018-02-14 15:21:20 +0800

    Update excel_extractor_test.py

--- excel_extractor_test.py
+++ excel_extractor_test.py
@@ -12,33 +12,27 @@ from openpyxl import Workbook
 os.chdir(r"C:\Users\shaolqi\Desktop")
 print('Opening workbook...')
 wb = openpyxl.load_workbook('免征车辆购置税的新能源汽车车型目录（第十五批）.xlsx')
-sheet = wb.get_sheet_by_name('Table 1')
+sheet = wb.get_sheet_by_name('Table 2')
 # ------------create a new workbook to store the selected data
 wbTemp = Workbook()
 sheetTemp = wbTemp.create_sheet('newEnergyCarList', 0)
 # print(type(sheet))
-
 # ------------------------#customize the rectangle area you want-----------------------
 # startRow    = int(input("enter the start row number you want:"))
 # startColumn = input("enter the start column letter you want:")
 # endRow      = int(input("enter the end row number you want:"))
 # endColumn   = input("enter the end column letter you want:")
-#
 # startCell = startColumn + str(startRow)
 # endCell   = endColumn + str(endRow)
-startCell = 'A5'
-endCell = 'L28'
-#
+startCell = 'A2'
+endCell = 'J72'
 recArea = sheet[startCell:endCell]  # recArea type is tuple
 # listRecArea = list(recArea)
-
 # print(recArea[0][1].value)
 # Loop through recArea --------------------------------------------------------------------------------------
 dictData = {}
 lsEachDictKey = []
-
 lsEachDictValueAll = []
-
 for rowNum in range(0,len(recArea)):                     #iterate all row in recArea
     if rowNum == 0: #如果是第一行，则存为key
         for eachCell in recArea[rowNum]:
@@ -52,16 +46,14 @@ for rowNum in range(0,len(recArea)):                     #iterate all row in rec
             lsEachDictValueAll.append(eachCell.value)
 
     # dictData.setdefault(tupEachDictKey,lsEachDictValue)
-
-
+# pprint.pprint(lsEachDictKey)
+#regroup lsEachDictValue-------------------------------------
 lsEachDictValueAllRegroup = []
-
-for c in range(0,len(lsEachDictKey)):        #regroup lsEachDictValue...but will there be any error in using variable c?
+for c in range(0,len(lsEachDictKey)):    # Will there be any error in using variable c?
     while c < len(lsEachDictValueAll):
         lsEachDictValueAllRegroup.append(lsEachDictValueAll[c])
-        c = c + len(lsEachDictKey)
-    c = c + 1
-
+        c += len(lsEachDictKey)
+    c += 1
 pprint.pprint(lsEachDictValueAllRegroup)
 
 
