f20ba789eb9cb498c2d9dc81399e5f86d2164424
RicheyShaw <rickyshao1988@foxmail.com>
2018-03-02 10:54:47 +0800

    Update writeNewEnergyCarCatalogExcel.py

--- writeNewEnergyCarCatalogExcel.py
+++ writeNewEnergyCarCatalogExcel.py
@@ -9,15 +9,20 @@ os.chdir('C:\\Users\\shaolqi\\PycharmProjects\\excel_extractor')
 import openpyxl, newCatalog4NEC
 from openpyxl.utils import get_column_letter
 # ---------------------create a new workbook to store newCatalog4NEC.py
-destFileName = '自用新能源车型目录.xlsx'
+destFileName = 'catalog_of_new_energy_car.xlsx'
 wbUpdate = openpyxl.load_workbook(destFileName)
 wsUpdate = wbUpdate.active
 
-lsWsColValueAll = []  # iterate all contents in first row of 
-for m in range(1, wsUpdate.max_column+1):
-    lsWsColValueAll.append(wsUpdate[get_column_letter(m) + '1'].value)
-tupSheetColValueAll = tuple(lsWsColValueAll)
-# print(lsWsColValueAll)
-# lsKeyIn = []
-# for n in newCatalog4NEC.allData.keys():
-# #     if newCatalog4NEC.allData[n]
+
+lsWs1stRowValueAll = []  # iterate all contents in first row of wsUpdate
+for m in range(1, wsUpdate.max_column + 1):
+    lsWs1stRowValueAll.append(wsUpdate[get_column_letter(m) + '1'].value)
+tupSheetColValueAll = tuple(lsWs1stRowValueAll)
+print(lsWs1stRowValueAll)
+
+for eachKey in newCatalog4NEC.allData.keys():
+    if eachKey in lsWs1stRowValueAll:
+        pinPoint = lsWs1stRowValueAll.index(eachKey)
+        for n in range(0, len(newCatalog4NEC.allData[eachKey])):
+            wsUpdate.cell(row = n + 2, column = pinPoint + 1).value = newCatalog4NEC.allData[eachKey][n]
+wbUpdate.save(filename='new_file.xlsx')
