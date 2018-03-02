cd8d05ce58c465ad9101becf7642a9e673fbe10c
RicheyShaw <rickyshao1988@foxmail.com>
2018-03-02 14:11:45 +0800

    Update writeNewEnergyCarCatalogExcel.py

--- writeNewEnergyCarCatalogExcel.py
+++ writeNewEnergyCarCatalogExcel.py
@@ -12,17 +12,23 @@ from openpyxl.utils import get_column_letter
 destFileName = 'catalog_of_new_energy_car.xlsx'
 wbUpdate = openpyxl.load_workbook(destFileName)
 wsUpdate = wbUpdate.active
+# ----------------------------------------------------------------------------
+rowNumMax = wsUpdate.max_row  # auto adding towards '序号'
+for n in range(0, len(newCatalog4NEC.allData['序号'])):
+    newCatalog4NEC.allData['序号'][n]= newCatalog4NEC.allData['序号'][n] + rowNumMax - 1
 
 
-lsWs1stRowValueAll = []  # iterate all contents in first row of wsUpdate
+lsWs1stRowValueAll = []  # iterate all contents in 1st. row of wsUpdate EVERY TIME, in case adding new item in the
+#  future
 for m in range(1, wsUpdate.max_column + 1):
     lsWs1stRowValueAll.append(wsUpdate[get_column_letter(m) + '1'].value)
-tupSheetColValueAll = tuple(lsWs1stRowValueAll)
-print(lsWs1stRowValueAll)
+tupWs1stRowValueAll = tuple(lsWs1stRowValueAll)
+print(tupWs1stRowValueAll)
 
-for eachKey in newCatalog4NEC.allData.keys():
-    if eachKey in lsWs1stRowValueAll:
-        pinPoint = lsWs1stRowValueAll.index(eachKey)
+for eachKey in newCatalog4NEC.allData.keys():  # write dictData to current worksheet
+    if eachKey in tupWs1stRowValueAll:
+        pinPoint = tupWs1stRowValueAll.index(eachKey)
         for n in range(0, len(newCatalog4NEC.allData[eachKey])):
-            wsUpdate.cell(row = n + 2, column = pinPoint + 1).value = newCatalog4NEC.allData[eachKey][n]
-wbUpdate.save(filename='new_file.xlsx')
+            wsUpdate.cell(row =rowNumMax + n + 1, column =pinPoint + 1).value = newCatalog4NEC.allData[eachKey][n]
+
+wbUpdate.save(filename=destFileName)
